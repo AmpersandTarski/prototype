d45d2ed43848ab939468548460f618dbb34f7146
Michiel Stornebrink <>
2018-10-11 08:14:25 +0200

    PSR2 white space warning

--- src/Ampersand/Session.php
+++ src/Ampersand/Session.php
@@ -119,7 +119,7 @@ class Session
         
         // Update session variable. This is needed because windows platform doesn't seem to update the read time of the session file
         // which will cause a php session timeout after the default timeout of (24min), regardless of user activity. By updating the
-        // session file (updating 'lastAccess' variable) we ensure the the session file timestamps are updated on every request. 
+        // session file (updating 'lastAccess' variable) we ensure the the session file timestamps are updated on every request.
         $_SESSION['lastAccess'] = time();
         // Update lastAccess time also in plug/database to allow to use this aspect in Ampersand models
         $this->sessionAtom->link(date(DATE_ATOM, $_SESSION['lastAccess']), 'lastAccess[SESSION*DateTime]', false)->add();
