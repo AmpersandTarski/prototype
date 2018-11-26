20be5ec69726878bdabae45f882abe78c6713830
Michiel Stornebrink <>
2018-11-26 21:10:43 +0100

    Move logging setup to config file

--- bootstrap/project.php
+++ bootstrap/project.php
@@ -1,15 +1,11 @@
 <?php
 
 use Ampersand\Log\Logger;
-use Ampersand\Log\RequestIDProcessor;
 use Ampersand\Misc\Settings;
 use Ampersand\AmpersandApp;
 use Ampersand\Model;
 use Ampersand\AngularApp;
-use Monolog\Logger as MonoLogger;
-use Monolog\Handler\FingersCrossedHandler;
-use Monolog\Handler\RotatingFileHandler;
-use Monolog\Processor\WebProcessor;
+use Cascade\Cascade;
 
 date_default_timezone_set('Europe/Amsterdam'); // see http://php.net/manual/en/timezones.php for a list of supported timezones
 set_time_limit(30); // execution time limit is set to a default of 30 seconds. Use 0 to have no time limit. (not advised)
@@ -27,21 +23,7 @@ $debugMode = $settings->get('global.debugMode');
 error_reporting(E_ALL & ~E_NOTICE);
 ini_set("display_errors", false);
 
-// Add all channels to error and debug log file handlers
-$errorLog = new RotatingFileHandler(__DIR__ . '/log/error.log', 0, MonoLogger::DEBUG);
-// $errorLog->pushProcessor(new RequestIDProcessor())->pushProcessor(new WebProcessor(null, [ 'ip' => 'REMOTE_ADDR', 'method' => 'REQUEST_METHOD', 'url' => 'REQUEST_URI'])); // Adds IP adres and url info to log records
-$errorLog = new FingersCrossedHandler($errorLog, MonoLogger::ERROR, 0, true, true, MonoLogger::WARNING);
-$debugLog = new RotatingFileHandler(__DIR__ . '/log/debug.log', 0, MonoLogger::DEBUG);
-Logger::setFactoryFunction(function ($channel) use ($errorLog, $debugLog, $debugMode) {
-    $handlers[] = $errorLog;
-    if ($debugMode) {
-        $handlers[] = $debugLog;
-    }
-    return new MonoLogger($channel, $handlers);
-});
-
-// ExecEngine log
-Logger::getLogger('EXECENGINE')->pushHandler(new RotatingFileHandler(__DIR__ . '/log/execengine.log', 0, MonoLogger::DEBUG));
+Cascade::fileConfig(dirname(__FILE__, 2) . '/config/logging.yaml'); // loads logging configuration
 
 /**************************************************************************************************
  * APPLICATION
