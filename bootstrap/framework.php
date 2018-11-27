82e510d50dd3ab31b141e81572aa273275fa8b72
Michiel Stornebrink <>
2018-11-27 12:42:56 +0100

    Rename folder inside bootstrap folder.

--- bootstrap/framework.php
+++ bootstrap/framework.php
@@ -34,6 +34,6 @@ require_once(__DIR__ . '/project.php');
 
 // ExecEngine functions
 chdir(__DIR__);
-foreach (glob('execfunctions/*.php') as $filepath) {
+foreach (glob('files/*.php') as $filepath) {
     require_once(__DIR__ . DIRECTORY_SEPARATOR . $filepath);
 }
