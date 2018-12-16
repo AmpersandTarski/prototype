1fad4395c006c39ad2f89a192b1469a46b27fc1e
Michiel Stornebrink <>
2018-12-16 12:42:37 +0100

    Bugfix ResourcePath in case of null

--- src/Ampersand/Interfacing/ResourcePath.php
+++ src/Ampersand/Interfacing/ResourcePath.php
@@ -14,8 +14,12 @@ namespace Ampersand\Interfacing;
  */
 class ResourcePath
 {
-    public static function makePathList(string $path): array
+    public static function makePathList(string $path = null): array
     {
+        if (is_null($path)) {
+            $path = '';
+        }
+
         $path = trim($path, '/'); // remove root slash (e.g. '/Projects/xyz/..') and trailing slash (e.g. '../Projects/xyz/')
         
         if ($path === '') {
