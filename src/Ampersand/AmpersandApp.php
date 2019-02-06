5c694101d1edd846d1a7754428019270ace1273a
Michiel Stornebrink <michielstornebrink@gmail.com>
2019-02-06 21:31:55 +0100

    Add interface isAPI and label meta population

--- src/Ampersand/AmpersandApp.php
+++ src/Ampersand/AmpersandApp.php
@@ -492,28 +492,44 @@ class AmpersandApp
             $cpt->clearAtomCache(); // local cache in Ampersand code
         }
 
-        // Navigation menu items
+        // Meta population
         try {
             $transaction = $this->newTransaction();
 
-            // MainNavbar (i.e. all interfaces with SESSION as src concept)
+            // Add interfaces
+            foreach (Ifc::getAllInterfaces() as $ifc) {
+                /** @var \Ampersand\Interfacing\Ifc $ifc */
+                $ifcAtom = Atom::makeAtom($ifc->getId(), 'PF_Interface')->add();
+                $ifcAtom->link($ifc->getLabel(), 'label[PF_Interface*PF_Label]')->add();
+                foreach ($ifc->getRoleNames() as $roleName) {
+                    $ifcAtom->link($roleName, 'ifcRoles[PF_Interface*PF_Role]')->add();
+                }
+                if ($ifc->isPublic()) {
+                    $ifcAtom->link($ifcAtom, 'isPublic[PF_Interface*PF_Interface]')->add();
+                }
+                if ($ifc->isAPI()) {
+                    $ifcAtom->link($ifcAtom, 'isAPI[PF_Interface*PF_Interface]')->add();
+                }
+            }
+
+            // Add menu items
             $mainMenu = Atom::makeAtom('MainMenu', 'PF_NavMenu')->add();
             $mainMenu->link('Main menu', 'label[PF_NavMenuItem*PF_Label]')->add();
             $mainMenu->link($mainMenu, 'isPartOf[PF_NavMenuItem*PF_NavMenu]')->add();
             $i = '0';
             foreach (Ifc::getAllInterfaces() as $ifc) {
                 /** @var \Ampersand\Interfacing\Ifc $ifc */
+                // Skip API interfaces
+                if ($ifc->isAPI()) {
+                    continue;
+                }
+
+                // MainMenu (i.e. all interfaces with SESSION as src concept)
                 if ($ifc->getSrcConcept()->isSession()) {
                     $i++;
                     $menuItem = Atom::makeAtom($ifc->getId(), 'PF_NavMenuItem')->add();
                     $menuItem->link($ifc->getLabel(), 'label[PF_NavMenuItem*PF_Label]')->add();
-                    $ifcAtom = $menuItem->link($ifc->getId(), 'ifc[PF_NavMenuItem*PF_Interface]')->add()->tgt();
-                    foreach ($ifc->getRoleNames() as $roleName) {
-                        $ifcAtom->link($roleName, 'ifcRoles[PF_Interface*PF_Role]')->add();
-                    }
-                    if ($ifc->isPublic()) {
-                        $ifcAtom->link($ifcAtom, 'isPublic[PF_Interface*PF_Interface]')->add();
-                    }
+                    $menuItem->link($ifc->getId(), 'ifc[PF_NavMenuItem*PF_Interface]')->add();
                     $menuItem->link($i, 'seqNr[PF_NavMenuItem*PF_SeqNr]')->add();
                     $menuItem->link($mainMenu, 'isSubItemOf[PF_NavMenuItem*PF_NavMenuItem]')->add();
                 }
@@ -521,10 +537,10 @@ class AmpersandApp
 
             $transaction->runExecEngine()->close(false, false);
             if ($transaction->isRolledBack()) {
-                throw new Exception("Navigation menu items do not satisfy invariant rules. See log files", 500);
+                throw new Exception("Meta population does not satisfy invariant rules. See log files", 500);
             }
         } catch (Exception $e) {
-            throw new Exception("Error in installing navigation menu items: {$e->getMessage()}", 500, $e);
+            throw new Exception("Error in installing meta population: {$e->getMessage()}", 500, $e);
         }
 
         // Default population
