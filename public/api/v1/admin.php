f98e30f0e78eaa5e789a5f9d634a1f70a9232f1a
Michiel Stornebrink <michielstornebrink@gmail.com>
2019-02-07 18:05:57 +0100

    Move API code regarding installer to seperate file

--- public/api/v1/admin.php
+++ public/api/v1/admin.php
@@ -17,7 +17,6 @@ use Symfony\Component\Serializer\Encoder\CsvEncoder;
 use Ampersand\Core\Concept;
 use Ampersand\Core\Atom;
 use Ampersand\IO\RDFGraph;
-use Ampersand\Misc\Installer;
 
 /**
  * @var \Slim\App $api
@@ -104,75 +103,6 @@ $api->group('/admin', function () {
         $transaction->runExecEngine()->close();
 
         return $response->withJson($list, 200, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
-    });
-
-    /**
-     * @phan-closure-scope \Slim\Container
-     */
-    $this->get('/installer', function (Request $request, Response $response, $args = []) {
-        /** @var \Slim\Container $this */
-        /** @var \Ampersand\AmpersandApp $ampersandApp */
-        $ampersandApp = $this['ampersand_app'];
-
-        if ($ampersandApp->getSettings()->get('global.productionEnv')) {
-            throw new Exception("Reinstallation of application not allowed in production environment", 403);
-        }
-        
-        $defaultPop = filter_var($request->getQueryParam('defaultPop'), FILTER_VALIDATE_BOOLEAN, FILTER_NULL_ON_FAILURE) ?? true;
-        $ignoreInvariantRules = filter_var($request->getQueryParam('ignoreInvariantRules'), FILTER_VALIDATE_BOOLEAN, FILTER_NULL_ON_FAILURE) ?? false;
-
-        $ampersandApp
-            ->reinstall($defaultPop, $ignoreInvariantRules) // reinstall and initialize application
-            ->setSession();
-
-        $ampersandApp->checkProcessRules(); // Check all process rules that are relevant for the activate roles
-
-        $content = $ampersandApp->userLog()->getAll(); // Return all notifications
-
-        return $response->withJson($content, 200, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
-    })->setName('applicationInstaller');
-
-    /**
-     * @phan-closure-scope \Slim\Container
-     */
-    $this->get('/installer/metapopulation', function (Request $request, Response $response, $args = []) {
-        /** @var \Slim\Container $this */
-        /** @var \Ampersand\AmpersandApp $ampersandApp */
-        $ampersandApp = $this['ampersand_app'];
-
-        $allowedRoles = $ampersandApp->getSettings()->get('rbac.adminRoles');
-        if (!$ampersandApp->hasRole($allowedRoles)) {
-            throw new Exception("You do not have access to install metapopulation", 403);
-        }
-
-        $installer = new Installer($ampersandApp, Logger::getLogger('APPLICATION'));
-        $installer->reinstallMetaPopulation();
-
-        $ampersandApp->checkProcessRules(); // Check all process rules that are relevant for the activate roles
-        $content = $ampersandApp->userLog()->getAll(); // Return all notifications
-
-        return $response->withJson($content, 200, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
-    });
-
-    /**
-     * @phan-closure-scope \Slim\Container
-     */
-    $this->get('/installer/checksum/update', function (Request $request, Response $response, $args = []) {
-        /** @var \Slim\Container $this */
-        /** @var \Ampersand\AmpersandApp $ampersandApp */
-        $ampersandApp = $this['ampersand_app'];
-
-        if ($ampersandApp->getSettings()->get('global.productionEnv')) {
-            throw new Exception("Checksum update is not allowed in production environment", 403);
-        }
-
-        $ampersandApp->getModel()->writeChecksumFile();
-        
-        $ampersandApp->userLog()->info('New checksum calculated for generated Ampersand model files');
-
-        $content = $ampersandApp->userLog()->getAll(); // Return all notifications
-
-        return $response->withJson($content, 200, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
     });
 
     /**
