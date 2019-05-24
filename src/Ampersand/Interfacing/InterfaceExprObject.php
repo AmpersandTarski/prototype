fa3d5c91ebd88a91a0fd875c90ad229173bb7542
Michiel Stornebrink <michielstornebrink@gmail.com>
2019-05-24 13:27:54 +0200

    Bugfix query optimization for reference interfaces

--- src/Ampersand/Interfacing/InterfaceExprObject.php
+++ src/Ampersand/Interfacing/InterfaceExprObject.php
@@ -889,12 +889,12 @@ class InterfaceExprObject extends AbstractIfcObject implements InterfaceObjectIn
 
         // If interface isIdent (i.e. expr = I[Concept]), and no epsilon is required (i.e. srcConcept equals tgtConcept of parent ifc) we can return the src
         // However, if query for this expression contains sub data, it is more efficient to evaluate the query (see Issue #217)
-        if ($this->isIdent() && $this->srcConcept === $src->concept && !$this->queryContainsSubData) {
+        if ($this->isIdent() && !$this->isRef() && $this->srcConcept === $src->concept && !$this->queryContainsSubData) {
             $tgts[] = $src;
         } else {
             // Try to get tgt atom from src query data (in case of uni relation in same table)
             $tgtId = $src->getQueryData('ifc_' . $this->id, $exists); // column is prefixed with ifc_ in query data
-            if ($exists && !$this->queryContainsSubData) {
+            if ($exists && !$this->queryContainsSubData && !$this->isRef()) {
                 if (!is_null($tgtId)) {
                     $tgts[] = new Atom($tgtId, $this->tgtConcept);
                 }
