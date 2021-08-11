f28f4b13082c6d1e9e8c3ebbfb1c7c4a67d759f8
Michiel Stornebrink <michielstornebrink@gmail.com>
2021-08-11 11:27:37 +0200

    Bugfix previous commit

--- src/Ampersand/Interfacing/ResourceList.php
+++ src/Ampersand/Interfacing/ResourceList.php
@@ -173,16 +173,16 @@ class ResourceList
         
         $newResource = $this->makeResource($this->ifcObject->create($this->srcAtom));
 
-        // Check if file is specified. This is not the case e.g. when post_max_size is exceeded
-        // Maximum post size is checked in generic API middleware function
-        if (!isset($_FILES['file'])) {
-            throw new Exception("No file(s) provided to upload", 400);
-        }
-
-        $fileInfo = $_FILES['file'];
-
         // Special case for file upload
         if ($newResource->concept->isFileObject()) {
+            // Check if file is specified. This is not the case e.g. when post_max_size is exceeded
+            // Maximum post size is checked in generic API middleware function
+            if (!isset($_FILES['file'])) {
+                throw new Exception("No file(s) provided to upload", 400);
+            }
+
+            $fileInfo = $_FILES['file'];
+            
             if (is_uploaded_file($fileInfo['tmp_name'])) {
                 $fs = $ampersandApp->fileSystem();
                 
