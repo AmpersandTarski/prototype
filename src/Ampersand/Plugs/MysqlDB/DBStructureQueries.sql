6f8fe42bae3efd28048674ea09d76f3a27b5db91
Michiel Stornebrink <michielstornebrink@gmail.com>
2021-10-19 22:09:27 +0200

    Add database table to store model version history

--- src/Ampersand/Plugs/MysqlDB/DBStructureQueries.sql
+++ src/Ampersand/Plugs/MysqlDB/DBStructureQueries.sql
@@ -11,3 +11,13 @@ CREATE TABLE "__conj_violation_cache__"
     , ROW_FORMAT = DYNAMIC
 ;
 CREATE INDEX "__cvc__conjId" ON "__conj_violation_cache__" ("conjId");
+
+DROP TABLE IF EXISTS "__ampersand_model_history__";
+CREATE TABLE "__ampersand_model_history__"
+    ( "id" INT NOT NULL AUTO_INCREMENT
+    , "compilerVersion" VARCHAR(255) NOT NULL
+    , "checksum" VARCHAR(255) NOT NULL
+    , "installed_on" TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP
+    ) ENGINE     = InnoDB DEFAULT CHARACTER SET UTF8MB4 COLLATE UTF8MB4_NOPAD_BIN
+    , ROW_FORMAT = DYNAMIC
+;
