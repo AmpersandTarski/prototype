efacfac6b7c5991f172f212a5cf5301b5a67889d
hanjoosten <han.joosten@ordina.nl>
2023-01-22 18:40:18 +0000

    Initial sidebar for prototype

---
+++ docs/sidebar.js
@@ -0,0 +1,22 @@
+module.exports = {
+
+
+    prototypeGuideSidebar: [
+        {
+            label: 'How to create a prototype',
+            type: 'category',
+            items: [
+                'prototype/README',
+                'prototype/config',
+                'prototype/file-system',
+                'prototype/event-dispatcher',
+                'prototype/interface-templates',
+                'prototype/generics',
+                'prototype/ampersand-compiler',
+            ]
+        },
+    ],
+    prototypeReferenceSidebar: [
+
+    ]
+};
