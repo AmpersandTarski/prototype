e5e0f2efff7aa4ffe7d4ce779112d6f3cb93c4ef
Michiel Stornebrink <michielstornebrink@gmail.com>
2023-07-10 22:36:01 +0200

    Add implementation for BoxPropButtonComponent

--- frontend/src/app/shared/box-components/box-prop-button/box-prop-button.component.ts
+++ frontend/src/app/shared/box-components/box-prop-button/box-prop-button.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
-
+import { BaseBoxComponent } from '../BaseBoxComponent.class';
+import { ObjectBase } from '../../objectBase.interface';
+type PropButtonItem = ObjectBase & {
+  label: string;
+  property: boolean;
+};
 @Component({
   selector: 'app-box-prop-button',
   templateUrl: './box-prop-button.component.html',
-  styleUrls: ['./box-prop-button.component.scss']
+  styleUrls: ['./box-prop-button.component.scss'],
 })
-export class BoxPropButtonComponent {
-
+export class BoxPropButtonComponent<TItem extends PropButtonItem, I> extends BaseBoxComponent<TItem, I> {
+  toggleProperty(item: TItem) {
+    this.interfaceComponent
+      .patch(item._path_, [{ op: 'replace', path: 'property', value: !item.property }])
+      .subscribe();
+  }
 }
