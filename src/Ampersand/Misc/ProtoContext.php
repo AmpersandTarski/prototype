254e2bbd0ef4b6d75be8fd27c25e76811c2f044d
Michiel Stornebrink <michielstornebrink@gmail.com>
2024-02-29 21:04:48 +0100

    Remove SESSION from prototype context namespace

--- src/Ampersand/Misc/ProtoContext.php
+++ src/Ampersand/Misc/ProtoContext.php
@@ -15,7 +15,7 @@ abstract class ProtoContext
         CPT_NAV_MENU        = 'PrototypeContext.NavMenu',
         CPT_NAV_ITEM        = 'PrototypeContext.NavMenuItem',
         CPT_DATETIME        = 'PrototypeContext.DateTime',
-        CPT_SESSION         = 'PrototypeContext.SESSION';
+        CPT_SESSION         = 'SESSION';
 
     const CONCEPTS = [
         self::CPT_ROLE,
@@ -34,9 +34,9 @@ abstract class ProtoContext
         REL_IFC_IS_API              = 'PrototypeContext.isAPI[PrototypeContext.Interface*PrototypeContext.Interface]',
         REL_IFC_LABEL               = 'PrototypeContext.label[PrototypeContext.Interface*PrototypeContext.Label]',
         REL_ROLE_LABEL              = 'PrototypeContext.label[PrototypeContext.Role*PrototypeContext.Label]',
-        REL_SESSION_ALLOWED_ROLES   = 'PrototypeContext.sessionAllowedRoles[PrototypeContext.SESSION*PrototypeContext.Role]',
-        REL_SESSION_ACTIVE_ROLES    = 'PrototypeContext.sessionActiveRoles[PrototypeContext.SESSION*PrototypeContext.Role]',
-        REL_SESSION_LAST_ACCESS     = 'PrototypeContext.lastAccess[PrototypeContext.SESSION*PrototypeContext.DateTime]',
+        REL_SESSION_ALLOWED_ROLES   = 'PrototypeContext.sessionAllowedRoles[SESSION*PrototypeContext.Role]',
+        REL_SESSION_ACTIVE_ROLES    = 'PrototypeContext.sessionActiveRoles[SESSION*PrototypeContext.Role]',
+        REL_SESSION_LAST_ACCESS     = 'PrototypeContext.lastAccess[SESSION*PrototypeContext.DateTime]',
         REL_NAV_LABEL               = 'PrototypeContext.label[PrototypeContext.NavMenuItem*PrototypeContext.Label]',
         REL_NAV_IS_VISIBLE          = 'PrototypeContext.isVisible[PrototypeContext.NavMenuItem*PrototypeContext.NavMenuItem]',
         REL_NAV_IS_PART_OF          = 'PrototypeContext.isPartOf[PrototypeContext.NavMenuItem*PrototypeContext.NavMenu]',
