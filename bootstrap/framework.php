030576ec62f26bcf4d636fdeb9a1db06f24b33ef
Simon Epskamp <simon@friendlypixel.nl>
2024-10-03 11:30:29 +0000

    Put monolog config in external php file so it can easily be overwritten downstream

--- bootstrap/framework.php
+++ bootstrap/framework.php
@@ -10,19 +10,12 @@ use Ampersand\API\Middleware\LogPerformanceMiddleware;
 use Ampersand\API\Middleware\PostMaxSizeMiddleware;
 use Ampersand\Frontend\AngularJSApp;
 use Ampersand\Log\Logger;
-use Ampersand\Log\RequestIDProcessor;
 use Ampersand\Misc\Settings;
 use Ampersand\Model;
 use Ampersand\Plugs\MysqlConjunctCache\MysqlConjunctCache;
 use Ampersand\Plugs\MysqlDB\MysqlDB;
 use League\Flysystem\Adapter\Local;
 use League\Flysystem\Filesystem;
-use Monolog\Handler\FingersCrossed\ErrorLevelActivationStrategy;
-use Monolog\Handler\FingersCrossedHandler;
-use Monolog\Handler\StreamHandler;
-use Monolog\Logger as MonologLogger;
-use Monolog\Processor\WebProcessor;
-use Monolog\Registry;
 use Slim\App;
 use Slim\Container;
 use Symfony\Component\EventDispatcher\EventDispatcher;
@@ -92,29 +85,8 @@ require_once($composerAutoloaderFile);
 /**************************************************************************************************
  * LOGGING
  *************************************************************************************************/
-// PHP log
-ini_set('error_reporting', E_ALL & ~E_NOTICE); // @phan-suppress-current-line PhanTypeMismatchArgumentInternal
-ini_set("display_errors", '0');
-ini_set("log_errors", '1');
 
-// Processors
-$webProcessor = new WebProcessor(extraFields: [
-    'ip' => 'REMOTE_ADDR',
-    'method' => 'REQUEST_METHOD',
-    'url' => 'REQUEST_URI',
-]);
-$requestIDProcessor = new RequestIDProcessor();
-$processors = [$requestIDProcessor, $webProcessor];
-// Handlers
-$stdoutStream = new StreamHandler('php://stdout', level: MonologLogger::DEBUG);
-$stdout = new FingersCrossedHandler($stdoutStream, activationStrategy: new ErrorLevelActivationStrategy(MonologLogger::ERROR), passthruLevel: MonologLogger::NOTICE);
-$stderr = new StreamHandler('php://stderr', level: MonologLogger::WARNING);
-$handlers = [$stdout, $stderr];
-// Loggers
-foreach (['EXECENGINE', 'IO', 'API', 'APPLICATION', 'DATABASE', 'CORE', 'RULEENGINE', 'TRANSACTION', 'INTERFACING'] as $name) {
-    $logger = new MonologLogger($name, $handlers, $processors);
-    Registry::addLogger($logger);
-}
+require_once('../config/logging.php');
 
 /**************************************************************************************************
  * AMPERSAND APPLICATION
