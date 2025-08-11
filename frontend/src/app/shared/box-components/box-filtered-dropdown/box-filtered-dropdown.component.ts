cd4194fbed7e443059fbc4a1e08ddc3587818458
Erwin van Lun <erwin@vanlun.com>
2025-08-11 15:46:37 +0200

    Implement filtered dropdown component for feature-254
    - Add BoxFilteredDropdownComponent with TypeScript implementation
    - Create HTML template with PrimeNG dropdown and filtering support
    - Support both single and multi-select modes
    - Add proper CRUD operations for dropdown selections
    - Integrate with Ampersand's BaseBoxComponent architecture

--- frontend/src/app/shared/box-components/box-filtered-dropdown/box-filtered-dropdown.component.ts
+++ frontend/src/app/shared/box-components/box-filtered-dropdown/box-filtered-dropdown.component.ts
@@ -4,7 +4,7 @@ import { BaseBoxComponent } from '../BaseBoxComponent.class';
 
 
 @Component({
-selector: 'app-box-filtered-dropdown',
+  selector: 'app-box-filtered-dropdown',
   templateUrl: './box-filtered-dropdown.component.html',
   styleUrls: ['./box-filtered-dropdown.component.scss'],
 })
@@ -12,9 +12,61 @@ export class BoxFilteredDropdownComponent<
   TItem extends ObjectBase,
   I extends ObjectBase | ObjectBase[],
 > extends BaseBoxComponent<TItem, I> {
-  // @Input() action?: 'toggle' | 'set' | 'clear' = 'toggle';
+  
+  /**
+   * Because we don't get correct isUni from template just check if prop is array
+   * to see if multiple or single value.
+   */
+  get propertyIsArray() {
+    return Array.isArray(this.resource[this.propertyName]);
+  }
+
+  get selectFrom() {
+    return this.resource['selectFrom'] ?? [];
+  }
+
+  dirty = false;
 
   onDropdownChange(event: any) {
-    // Handle dropdown change event
+    // Handle dropdown change event - mark as dirty and update value
+    this.dirty = true;
+    // The value update is handled by ngModel binding
+  }
+
+  updateValue() {
+    if (!this.dirty) return;
+
+    // transform empty string to null value
+    if (this.resource[this.propertyName] === '') {
+      this.resource[this.propertyName] = null;
+    }
+
+    const val = this.resource[this.propertyName];
+
+    this.interfaceComponent
+      .patch(this.resource._path_, [
+        {
+          op: 'replace',
+          path: this.propertyName,
+          // Send _id_ of object when present, primitive value otherwise
+          value: val?._id_ ? val._id_ : val,
+        },
+      ])
+      .subscribe(() => {
+        this.dirty = false;
+      });
+  }
+
+  addValue() {
+    const val = this.newItemControl.value as any;
+    if (val && val._id_) {
+      this.addItem();
+    }
+  }
+
+  removeItemByIndex(index: number) {
+    if (this.data && this.data[index]) {
+      this.removeItem(this.data[index]);
+    }
   }
 }
