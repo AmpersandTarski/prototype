466b55e64c67cf38d7077e8a9fc4ec75936253de
Stef Joosten <stef.joosten@ou.nl>
2025-09-23 22:07:08 +0200

    docs: add new prototype guide entries to sidebar navigation
    Add 5 new documentation sections to prototypeGuideSidebar:
    - README and Frontender Quick Start for getting started
    - Creating Custom BOX Templates and Documenting Prototype Changes for development workflows
    - Back End Services for backend integration guidance
    
    This expands the available prototype documentation and improves navigation structure for developers.

--- docs/sidebar.js
+++ docs/sidebar.js
@@ -1,9 +1,34 @@
 module.exports = {
   prototypeGuideSidebar: [
     {
+      label: "README",
+      type: "doc",
+      id: "prototype/README",
+    },
+    {
+      label: "Frontender Quick Start",
+      type: "doc",
+      id: "prototype/guides/frontender-quick-start",
+    },
+    {
       label: "BOX Template Development Guide",
       type: "doc",
       id: "prototype/guides/box-template-development-guide",
+    },
+    {
+      label: "Creating Custom BOX Templates",
+      type: "doc",
+      id: "prototype/guides/creating-custom-box-templates",
+    },
+    {
+      label: "Documenting Prototype Changes",
+      type: "doc",
+      id: "prototype/guides/documenting-prototype-changes",
+    },
+    {
+      label: "Back End Services",
+      type: "doc",
+      id: "prototype/guides/back-end-services",
     },
   ],
   prototypeReferenceSidebar: [
