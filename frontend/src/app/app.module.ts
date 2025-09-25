5b4ed7bac665f883ebb034ad22c97328c297ddc6
Erwin van Lun <erwin@vanlun.com>
2025-09-25 12:19:43 +0200

    now reading interfaces.json for tot, uni, crud and concept types

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
+import { NgModule, APP_INITIALIZER } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AdminModule } from './admin/admin.module';
@@ -13,6 +13,17 @@ import { ToastModule } from 'primeng/toast';
 import { CoreModule } from './core/core.module';
 import { MessageService } from 'primeng/api';
 import { ProjectModule } from './generated/project.module';
+import { InterfacesJsonService } from './shared/services/interfaces-json.service';
+
+/**
+ * Initializer function to load interfaces.json at app startup
+ * Will throw error if file is not available
+ */
+export function initializeInterfaces(interfacesJsonService: InterfacesJsonService): () => Promise<void> {
+  return (): Promise<void> => {
+    return interfacesJsonService.loadInterfaces();
+  };
+}
 
 @NgModule({
   declarations: [AppComponent],
@@ -29,7 +40,16 @@ import { ProjectModule } from './generated/project.module';
     AppRoutingModule,
     ToastModule,
   ],
-  providers: [httpInterceptorProviders, MessageService],
+  providers: [
+    httpInterceptorProviders,
+    MessageService,
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeInterfaces,
+      deps: [InterfacesJsonService],
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
